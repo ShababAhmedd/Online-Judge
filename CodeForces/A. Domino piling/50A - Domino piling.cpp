fbea9958a850c9cbbd5661870f457629f7b8c73b
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2022-10-07 12:09:18 +0600

    Update 50A - Domino piling.cpp

--- CodeForces/A. Domino piling/50A - Domino piling.cpp
+++ CodeForces/A. Domino piling/50A - Domino piling.cpp
@@ -1,1 +1,16 @@
-//cpp
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+ 
+	int a, b, prod, result;
+	cin >> a >> b;
+	prod = a * b;
+	result = prod / 2;
+	cout << result;
+ 
+	return 0;
+}
