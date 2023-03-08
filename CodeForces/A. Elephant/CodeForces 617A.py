c0a7c0112d241aeedafa5ddb3a9b6ac00634c87b
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2023-03-08 13:49:53 +0600

    Create CodeForces 617A.py

---
+++ CodeForces/A. Elephant/CodeForces 617A.py
@@ -0,0 +1,33 @@
+x = int(input())
+
+if 5 <= x:
+  y = x//5
+  z = x%5
+  if z != 0:
+    y += 1
+
+if 4 <= x:
+  y = x//5
+  z = x%5
+  if z != 0:
+    y += 1
+
+if 3 <= x:
+  y = x//5
+  z = x%5
+  if z != 0:
+    y += 1
+
+if 2 <= x:
+  y = x//5
+  z = x%5
+  if z != 0:
+    y += 1
+
+if 1 <= x:
+  y = x//5
+  z = x%5
+  if z != 0:
+    y += 1
+
+print(y)
