9ec88f25a21ca749d25037307d8471ed73e7c73d
Mashrur Safir Shabab <mashrur.safir.shabab@g.bracu.ac.bd>
2023-10-05 14:09:50 +0600

    Add files via upload

---
+++ AtCoder/B - Counting/AtCoder - abc209_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int A, B; cin >> A >> B;
+	if (A <= B) {
+		cout << B - A + 1 << '\n';
+	}
+	else {
+		cout << 0 << '\n';
+	}
+	return 0;
+}
