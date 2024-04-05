a8cd29ba0ee8c7797b62ce6f4267c14edabbfbdd
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-05 17:55:58 +0600

    Create Print It.c

---
+++ Phitron/Semester 1/Module 04/Assignment 01/Print It/Print It.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    printf("Hello, world! I am learning C programming language. ^_^\nProgramming is fun and challenging. /\\/\\/\\\nI want to give my 100%% dedication to learn!\tI will succeed one day.");
+    return 0;
+}
