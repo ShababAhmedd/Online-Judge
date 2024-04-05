59fe402d4b9c32117238c64cbe05a30d2ba3d997
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-05 17:57:46 +0600

    Create Multiply.c

---
+++ Phitron/Semester 1/Module 04/Assignment 01/Multiply/Multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    //int A, B;
+    long long int mul, A, B;
+    scanf("%lld %lld", &A, &B);
+    mul = A * B;
+    printf("%lld", mul);
+    return 0;
+}
