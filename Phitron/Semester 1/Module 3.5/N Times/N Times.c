b1305175d9aef844c742ba0ddec2ee5b92ae42ee
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-05 17:48:48 +0600

    Create N Times.c

---
+++ Phitron/Semester 1/Module 3.5/N Times/N Times.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n, i;
+    scanf("%d", &n);
+    
+    i = 0;
+    while(i < n){
+        printf("I Love Practice \n");
+        i++;
+    }
+    return 0;
+}
