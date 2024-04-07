adf682d14655c2307d7fe6731f868303948c0a8e
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-07 14:48:18 +0600

    Create I - Welcome for you with Conditions.c

---
+++ CodeForces/I - Welcome for you with Conditions.c
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/I
+#include <stdio.h>
+ 
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    if (A >= B){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+    return 0;
+}
