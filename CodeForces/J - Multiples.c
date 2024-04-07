b7ae25284393adeeaddcd13f80a668a4e4b1f954
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-07 15:06:47 +0600

    Create J - Multiples.c

---
+++ CodeForces/J - Multiples.c
@@ -0,0 +1,14 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
+#include <stdio.h>
+ 
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+    if (A%B==0 || B%A==0){
+    printf("Multiples");
+    }
+    else{
+        printf("No Multiples");
+    }
+    return 0;
+}
