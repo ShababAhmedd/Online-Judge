e63461adfa378dd931ddbefcee34160dc058bff1
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-15 17:19:49 +0600

    Create F. Multiplication table.c

---
+++ CodeForces/F. Multiplication table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int n, i;
+    scanf("%d", &n);
+
+    for (i=1; i<=12; i++){
+        printf("%d * %d = %d \n", n, i, n*i);
+    }
+
+    return 0;
+}
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
