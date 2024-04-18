262b22addcab2abd42908acb7e9550ace421fdd6
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-18 21:46:56 +0600

    Sheet #3 (Arrays)
    Assiut University Training - Newcomers
    Sheet #3 (Arrays)

---
+++ CodeForces/B. Searching.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    int n, i, j, k, x, ans;
+
+    scanf("%d", &n);
+    int a[n];
+
+    for (i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    ans = -1;
+    scanf("%d", &x);
+    for (j=0; j<n; j++){
+        if (a[j] == x){
+            ans = j;
+            break;
+        }
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
+
