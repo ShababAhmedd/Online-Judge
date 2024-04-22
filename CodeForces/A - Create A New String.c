ab93b38d04cc37d60546473d513f718851665c4e
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-04-23 02:33:18 +0600

    219856A - 17
    Assiut University Training - Newcomers
    Sheet #4 (Strings)

---
+++ CodeForces/A - Create A New String.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char s[1001];
+    char t[1001];
+
+    scanf("%s", s);
+    scanf("%s", t);
+
+    int s_size = strlen(s);
+    int t_size = strlen(t);
+
+    printf("%d %d\n", s_size, t_size);
+    printf("%s %s", s, t);
+
+    return 0;
+}
