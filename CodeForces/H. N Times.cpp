299a8e1d7f39b5ab02fccf572d115a9aaadde4c1
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-08-10 17:50:41 +0600

    Assiut University Training - Newcomers
    Sheet #5 (Functions)

---
+++ CodeForces/H. N Times.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        char c;
+        cin >> n >> c;
+
+        for (int i = 0; i < n; i++) {
+            cout << c << " " ;
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/H
