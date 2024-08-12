84da0f1de6af28835f55ab531d6729646bc8b3d9
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-08-12 21:14:27 +0600

    219158T - 12
    Assiut University Training - Newcomers
    Sheet #1 (Data type - Conditions)

---
+++ CodeForces/T. Sort Numbers.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+    int arr[3];
+    for (int i = 0; i < 3; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int a = arr[0];
+    int b = arr[1];
+    int c = arr[2];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    printf("\n");
+
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+
+
+
+    return 0;
+}
+
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/T
