7acf41e55278fea05f8ac2b7327cf937c9f1e313
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2024-08-19 01:37:20 +0600

    219158G - 16
    Assiut University Training - Newcomers
    Sheet #1 (Data type - Conditions)

---
+++ CodeForces/G. Summation from 1 to N.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << (n * (n + 1)) / 2;
+
+    return 0;
+}
+
+
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
