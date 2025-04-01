6cf744beb0be2f61d7eb115a0225558410a528a1
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-04-01 19:29:35 +0600

    Update invertedTriangularStarPattern.java

--- invertedTriangularStarPattern.java
+++ invertedTriangularStarPattern.java
@@ -15,3 +15,25 @@ public class learning {
     }
 
 }
+
+
+// =========================================================================
+
+
+public class learning {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = i; j <= 5; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
