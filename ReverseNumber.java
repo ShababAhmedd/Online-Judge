73a097083473950a8affdb8a10987e74aa617f51
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-04-01 21:12:34 +0600

    Reverse a Number Algorithm
    Reverse a Number Algorithm

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+public class learning {
+
+    public static void main(String[] args) {
+
+        int num = 7789, rev = 0;
+
+        while (num != 0) {
+            int rem = num % 10;
+            rev = rev*10 + rem;
+            num = num/10;
+        }
+
+        System.out.println(rev);
+
+    }
+
+}
