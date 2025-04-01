7391d12d5e50abf8aff1a6035d594b6bcb2d63e7
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-04-01 18:46:10 +0600

    Create squareNumberPattern.java

---
+++ squareNumberPattern.java
@@ -0,0 +1,20 @@
+public class learning {
+
+    public static void main(String[] args) {
+
+        int col = 5;
+        int row = 5;
+
+        for (int i = 1; i <= col; i++) {
+
+            for (int j = 1; j <= row; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
