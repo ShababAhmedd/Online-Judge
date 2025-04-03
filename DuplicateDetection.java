fab4becae7c1793959143d7931d41a2bb60cffec
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-04-04 00:48:51 +0600

    Brute Force Duplicate Detection Algorithm
    Brute Force Duplicate Detection Algorithm

---
+++ DuplicateDetection.java
@@ -0,0 +1,12 @@
+public class CheckDuplicateNumber {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4, 5, 1};
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i+1; j < num.length; j++) {
+                if (num[i] == num[j]) {
+                    System.out.println(num[i]);
+                }
+            }
+        }
+    }
+}
