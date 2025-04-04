c0e4974dea916584e58ec0c1ead0e8d15e9aee1f
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-04-04 14:26:56 +0600

    Variable Swapping Algorithm using a Temporary Variable
    Temporary Variable Swap
    Swapping with Temp Variable
    Variable Swapping (Temp Method)

---
+++ swappingUsingATempVar.java
@@ -0,0 +1,11 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+}
