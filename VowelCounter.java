0cf8b6aff8f86d410794180d7caee710d6ea163a
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-04-07 19:03:41 +0600

    Update VowelCounter.java

--- VowelCounter.java
+++ VowelCounter.java
@@ -1,17 +1,23 @@
 package prb_solving;
 
+import java.util.HashSet;
+
 public class FindVowel {
 
     public static void VowelCounter(String str) {
-        String vowels = "aeiou";
+        String vowels = "aeiouAEIOU";
         int count = 0;
         char[] charString = str.toCharArray();
+        HashSet<Character> hashSet = new HashSet<Character>();
         for(char c : charString) {
             if (vowels.indexOf(c) != -1) {
                 count++;
+                hashSet.add(c);
             }
         }
-        System.out.println(count);
+        System.out.println("Total number of vowels: " + count);
+        System.out.println("Total number of unique vowels: " + hashSet.size());
+        System.out.println("Unique Vowels: " + hashSet);
     }
 
     public static void main(String[] args) {
