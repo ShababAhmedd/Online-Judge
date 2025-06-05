191fcb71eedb82b03938138cd636c0658af90f10
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-06-05 17:07:35 +0600

    Update DuplicateDetection.java

--- DuplicateDetection.java
+++ DuplicateDetection.java
@@ -1,12 +1,23 @@
+import java.util.HashSet;
+
 public class CheckDuplicateNumber {
     public static void main(String[] args) {
-        int[] num = {1, 2, 3, 4, 5, 1};
+        int[] num = {1, 2, 3, 4, 5, 1, 1, 2};
+        HashSet<Integer> hashSet = new HashSet<>();
         for (int i = 0; i < num.length; i++) {
-            for (int j = i+1; j < num.length; j++) {
+            if (hashSet.contains(num[i])) {
+                continue;
+            }
+            int count = 1;
+            for (int j = i + 1; j < num.length; j++) {
                 if (num[i] == num[j]) {
-                    System.out.println(num[i]);
+                    count++;
                 }
             }
+            if (count > 1) {
+                System.out.println(num[i] + " - " + count);
+            }
+            hashSet.add(num[i]);
         }
     }
 }
