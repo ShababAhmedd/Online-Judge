6683bc4b21d634cb91c1b2dbb943b91ae1107c6f
Mashrur Safir Shabab <Shabab.ahmed2000sa@gmail.com>
2025-10-19 22:48:04 +0600

    Tuples.py
    https://www.hackerrank.com/challenges/python-tuples/problem
    
    Language: Python 2

---
+++ HackerRank/Tuples.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t = tuple(integer_list)
+    print(hash(t))
+
+# https://www.hackerrank.com/challenges/python-tuples/problem
